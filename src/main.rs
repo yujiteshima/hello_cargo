0a89c1cfd47b095e6fe8c14f9ecfb647c07adfc8
Yuji Teshima <yuji.teshima@gmail.com>
2022-08-08 21:38:59 +0900

    数当てゲーム作成

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+    loop{
+        let mut guess = String::new();
+        println!("Please input your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
     
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                println!("You guessed: {}", guess);
+                break;
+            }
+        }
+    }
 }
